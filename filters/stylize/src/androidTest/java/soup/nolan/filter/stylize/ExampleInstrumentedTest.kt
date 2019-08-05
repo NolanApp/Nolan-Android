cbdf170b0c89780fe44beaa02c0e198fed2f875e
SOUP <soupyong@gmail.com>
2019-08-05 10:10:28 +0900

    Stylize: Initialize module

---
+++ filters/stylize/src/androidTest/java/soup/nolan/filter/stylize/ExampleInstrumentedTest.kt
@@ -0,0 +1,23 @@
+package soup.nolan.filter.stylize
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [Testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+
+        assertEquals("soup.nolan.filter.stylize.test", appContext.packageName)
+    }
+}
