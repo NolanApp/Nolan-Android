8f5766fb630f69690187f89cb88aa6665f1070b1
SOUP <soupyong@gmail.com>
2019-08-07 01:18:34 +0900

    UI: Add animation for splash scene

--- app/src/main/java/soup/nolan/ui/splash/SplashFragment.kt
+++ app/src/main/java/soup/nolan/ui/splash/SplashFragment.kt
@@ -1,10 +1,11 @@
 package soup.nolan.ui.splash
 
+import android.animation.Animator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.postOnAnimationDelayed
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator
 import androidx.navigation.fragment.findNavController
 import soup.nolan.databinding.SplashFragmentBinding
 import soup.nolan.ui.base.BaseFragment
@@ -25,8 +26,26 @@ class SplashFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.postOnAnimationDelayed(1000) {
-            findNavController().navigate(SplashFragmentDirections.actionToCamera())
+        binding.logo.run {
+            alpha = 0f
+            scaleX = 0f
+            scaleY = 0f
+            animate()
+                .alpha(1f)
+                .scaleX(1f)
+                .scaleY(1f)
+                .setDuration(600)
+                .setInterpolator(FastOutSlowInInterpolator())
+                .setListener(object : Animator.AnimatorListener {
+
+                    override fun onAnimationCancel(animation: Animator) {}
+                    override fun onAnimationRepeat(animation: Animator) {}
+                    override fun onAnimationStart(animation: Animator) {}
+
+                    override fun onAnimationEnd(animation: Animator) {
+                        findNavController().navigate(SplashFragmentDirections.actionToCamera())
+                    }
+                })
         }
     }
 }
