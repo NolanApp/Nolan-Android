e46c11402251fc74cc94018a7f880cc0dffb1c97
Sungyong An <soupyong@gmail.com>
2019-10-02 17:47:53 +0900

    App: Adjust range of StyleTransfer
    from [0f, 1f] to [-1f, 1f].

--- app/src/main/java/soup/nolan/temp/StyleTransfer.kt
+++ app/src/main/java/soup/nolan/temp/StyleTransfer.kt
@@ -6,7 +6,6 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.ml.common.modeldownload.FirebaseLocalModel
 import com.google.firebase.ml.common.modeldownload.FirebaseModelManager
 import com.google.firebase.ml.custom.*
-import timber.log.Timber
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
@@ -16,7 +15,12 @@ object StyleTransfer {
     private const val STYLE_TRANSFORM = "style_transform"
 
     private const val IMAGE_SIZE = 512
-    private const val FLOAT_NUM_OF_BYTES_PER_CHANNEL = 4
+    private const val IMAGE_MEAN = 128f
+    private const val IMAGE_OFFSET = 1f
+    private const val FLOAT_TYPE_SIZE = 4
+    private const val PIXEL_SIZE = 3
+
+    private val intValues = IntArray(IMAGE_SIZE * IMAGE_SIZE)
 
     private val predictModel by lazy {
         val options = FirebaseModelOptions.Builder()
@@ -27,7 +31,7 @@ object StyleTransfer {
 
     private val predictOptions = FirebaseModelDataType.FLOAT32.let { dataType ->
         FirebaseModelInputOutputOptions.Builder()
-                .setInputFormat(0, dataType, intArrayOf(1, IMAGE_SIZE, IMAGE_SIZE, 3))
+                .setInputFormat(0, dataType, intArrayOf(1, IMAGE_SIZE, IMAGE_SIZE, PIXEL_SIZE))
                 .setOutputFormat(0, dataType, intArrayOf(1, 1, 1, 100))
                 .build()
     }
@@ -41,9 +45,9 @@ object StyleTransfer {
 
     private val transformOptions = FirebaseModelDataType.FLOAT32.let { dataType ->
         FirebaseModelInputOutputOptions.Builder()
-                .setInputFormat(0, dataType, intArrayOf(1, IMAGE_SIZE, IMAGE_SIZE, 3))
+                .setInputFormat(0, dataType, intArrayOf(1, IMAGE_SIZE, IMAGE_SIZE, PIXEL_SIZE))
                 .setInputFormat(1, dataType, intArrayOf(1, 1, 1, 100))
-                .setOutputFormat(0, dataType, intArrayOf(1, IMAGE_SIZE, IMAGE_SIZE, 3))
+                .setOutputFormat(0, dataType, intArrayOf(1, IMAGE_SIZE, IMAGE_SIZE, PIXEL_SIZE))
                 .build()
     }
 
@@ -79,33 +83,31 @@ object StyleTransfer {
                     transformModel.run(inputs, transformOptions)
                 }
                 .continueWith {
-                    val result = it.result!!.getOutput<Array<Array<Array<FloatArray>>>>(0)
-                    toBitmap(result)
+                    toBitmap(it.result!!.getOutput(0))
                 }
     }
 
-    private val intValues = IntArray(IMAGE_SIZE * IMAGE_SIZE)
     /**
      * Writes Image data into a `ByteBuffer`.
      */
     @Synchronized
     private fun Bitmap.toByteBuffer(): ByteBuffer {
         val pixels = centerCropped(IMAGE_SIZE).toPixels()
-        val imgData = ByteBuffer.allocateDirect(FLOAT_NUM_OF_BYTES_PER_CHANNEL * IMAGE_SIZE * IMAGE_SIZE * 3)
-        imgData.order(ByteOrder.nativeOrder())
-        imgData.rewind()
-        // Convert the image to int points.
-        var index = 0
-        for (i in 0 until IMAGE_SIZE) {
-            for (j in 0 until IMAGE_SIZE) {
-                val value = pixels[index++]
-                imgData.putFloat((value shr 16 and 0xFF) / 255f)
-                imgData.putFloat((value shr 8 and 0xFF) / 255f)
-                imgData.putFloat((value and 0xFF) / 255f)
-//                Timber.d("$i, $j = ${(value shr 16 and 0xFF) / 255f} ${(value shr 8 and 0xFF) / 255f} ${(value and 0xFF) / 255f}")
+        val inputSize = FLOAT_TYPE_SIZE * IMAGE_SIZE * IMAGE_SIZE * PIXEL_SIZE
+        return ByteBuffer.allocateDirect(inputSize).apply {
+            order(ByteOrder.nativeOrder())
+            rewind()
+
+            for (pixel in pixels) {
+                putFloat((pixel shr 16 and 0xFF) / IMAGE_MEAN - IMAGE_OFFSET) // R
+                putFloat((pixel shr 8 and 0xFF) / IMAGE_MEAN - IMAGE_OFFSET)  // G
+                putFloat((pixel and 0xFF) / IMAGE_MEAN - IMAGE_OFFSET)        // B
+//                Timber.d("$pixel =" +
+//                        " ${(pixel shr 16 and 0xFF) / IMAGE_MEAN - IMAGE_OFFSET}" +
+//                        " ${(pixel shr 8 and 0xFF) / IMAGE_MEAN - IMAGE_OFFSET}" +
+//                        " ${(pixel and 0xFF) / IMAGE_MEAN - IMAGE_OFFSET}")
             }
         }
-        return imgData
     }
 
     private fun Bitmap.centerCropped(size: Int): Bitmap {
@@ -124,17 +126,22 @@ object StyleTransfer {
         return intValues
     }
 
-    private fun toBitmap(result: Array<Array<Array<FloatArray>>>): Bitmap {
+    private fun toBitmap(output: Array<Array<Array<FloatArray>>>): Bitmap {
         var index = 0
         for (i in 0 until IMAGE_SIZE) {
             for (j in 0 until IMAGE_SIZE) {
                 intValues[index++] = (-0x1000000 or
-                        (result[0][i][j][0] * 255).toInt().shl(16) or
-                        (result[0][i][j][1] * 255).toInt().shl(8) or
-                        (result[0][i][j][2] * 255).toInt())
-//                Timber.d("$i, $j = ${result[0][i][j][0]} ${result[0][i][j][1]} ${result[0][i][j][2]}")
+                        ((output[0][i][j][0] + IMAGE_OFFSET) * IMAGE_MEAN).toInt().shl(16) or
+                        ((output[0][i][j][1] + IMAGE_OFFSET) * IMAGE_MEAN).toInt().shl(8) or
+                        ((output[0][i][j][2] + IMAGE_OFFSET) * IMAGE_MEAN).toInt())
             }
         }
+//        val i = 0
+//        val j = 0
+//        val R = ((output[0][i][j][0] + IMAGE_OFFSET) * IMAGE_MEAN).toInt().shl(16)
+//        val G = ((output[0][i][j][1] + IMAGE_OFFSET) * IMAGE_MEAN).toInt().shl(8)
+//        val B = ((output[0][i][j][2] + IMAGE_OFFSET) * IMAGE_MEAN).toInt()
+//        Timber.d("$i, $j = $R $G $B")
         return Bitmap.createBitmap(IMAGE_SIZE, IMAGE_SIZE, Bitmap.Config.ARGB_8888).apply {
             setPixels(intValues, 0, width, 0, 0, width, height)
         }
