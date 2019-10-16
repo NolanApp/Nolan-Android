05a28dbe357c597ddfe938dea0d100e52dfeddd1
Sungyong An <soupyong@gmail.com>
2019-10-17 00:17:00 +0900

    App: Use -ktx api to inject ViewModels
    Refer to https://android-review.googlesource.com/c/platform/frameworks/support/+/1009889

--- app/src/main/java/soup/nolan/ui/base/BaseFragment.kt
+++ app/src/main/java/soup/nolan/ui/base/BaseFragment.kt
@@ -1,12 +1,10 @@
 package soup.nolan.ui.base
 
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import dagger.android.support.DaggerFragment
-import soup.nolan.ui.utils.activityViewModelProvider
-import soup.nolan.ui.utils.lazyFast
-import soup.nolan.ui.utils.parentViewModelProvider
-import soup.nolan.ui.utils.viewModelProvider
 import javax.inject.Inject
 
 abstract class BaseFragment : DaggerFragment() {
@@ -14,12 +12,15 @@ abstract class BaseFragment : DaggerFragment() {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
-    protected inline fun <reified VM : ViewModel> activityViewModel(): Lazy<VM> =
-        lazyFast { activityViewModelProvider<VM>(viewModelFactory) }
+    protected inline fun <reified VM : ViewModel> activityViewModel(): Lazy<VM> {
+        return activityViewModels { viewModelFactory }
+    }
 
-    protected inline fun <reified VM : ViewModel> viewModel(): Lazy<VM> =
-        lazyFast { viewModelProvider<VM>(viewModelFactory) }
+    protected inline fun <reified VM : ViewModel> viewModel(): Lazy<VM> {
+        return viewModels { viewModelFactory }
+    }
 
-    protected inline fun <reified VM : ViewModel> parentViewModel(): Lazy<VM> =
-        lazyFast { parentViewModelProvider<VM>(viewModelFactory) }
+    protected inline fun <reified VM : ViewModel> parentViewModel(): Lazy<VM> {
+        return requireParentFragment().viewModels { viewModelFactory }
+    }
 }
