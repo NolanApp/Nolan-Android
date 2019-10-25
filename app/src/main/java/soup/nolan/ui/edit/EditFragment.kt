c1d99f4643bbcfca033b420525238bdd863a7b6b
Sungyong An <soupyong@gmail.com>
2019-10-26 00:46:13 +0900

    Stylize: Add new pop style for experimental module

--- app/src/main/java/soup/nolan/ui/edit/EditFragment.kt
+++ app/src/main/java/soup/nolan/ui/edit/EditFragment.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.net.toFile
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import soup.nolan.R
 import soup.nolan.databinding.EditFragmentBinding
+import soup.nolan.stylize.experimental.PopStyleTransfer
 import soup.nolan.stylize.experimental.StyleTransfer
 import soup.nolan.ui.base.BaseFragment
 import soup.nolan.ui.edit.EditFragmentDirections.Companion.actionToShare
@@ -20,6 +23,8 @@ class EditFragment : BaseFragment() {
 
     private val args: EditFragmentArgs by navArgs()
     private val viewModel: EditViewModel by viewModel()
+
+    private val transfer by lazy { PopStyleTransfer() }
 
     private lateinit var binding: EditFragmentBinding
 
@@ -35,22 +40,25 @@ class EditFragment : BaseFragment() {
     }
 
     private fun initViewState(binding: EditFragmentBinding) {
-        binding.editImageView.setImageURI(args.fileUri)
         binding.shareButton.setOnDebounceClickListener {
             findNavController().navigate(actionToShare(args.fileUri))
         }
         val style = BitmapFactory.decodeResource(resources, R.drawable.style)
-        val input = BitmapFactory.decodeFile(args.fileUri.toFile().path)
+        val input = FirebaseVisionImage.fromFilePath(requireContext(), args.fileUri).bitmap
+        binding.editImageView.setImageBitmap(input)
         val start = System.currentTimeMillis()
-        StyleTransfer.transform(style, input)
+        transfer.transform(input)
             .addOnSuccessListener {
-                Timber.d("success: $it ${System.currentTimeMillis() - start}ms")
+                val duration = System.currentTimeMillis() - start
+                Timber.d("success: $it $duration ms")
                 activity?.runOnUiThread {
                     binding.editImageView.setImageBitmap(it)
                 }
+                Toast.makeText(context, "Success! ($duration ms)", Toast.LENGTH_SHORT).show()
             }
             .addOnFailureListener {
                 Timber.d("failure: $it")
+                Toast.makeText(context, "Error: $it", Toast.LENGTH_SHORT).show()
             }
     }
 }
---
+++ filter/stylize-experimental/src/main/java/soup/nolan/stylize/experimental/PopStyleTransfer.kt
@@ -0,0 +1,117 @@
+package soup.nolan.stylize.experimental
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import com.google.android.gms.tasks.Task
+import com.google.firebase.ml.common.modeldownload.FirebaseLocalModel
+import com.google.firebase.ml.common.modeldownload.FirebaseModelManager
+import com.google.firebase.ml.custom.*
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+class PopStyleTransfer {
+
+    companion object {
+
+        private const val STYLE_TRANSFORM = "pop_art_styler"
+
+        private const val IMAGE_SIZE = 512
+        private const val IMAGE_MEAN = 128f
+        private const val IMAGE_OFFSET = 1f
+        private const val FLOAT_TYPE_SIZE = 4
+        private const val PIXEL_SIZE = 3
+    }
+
+    private val intValues = IntArray(IMAGE_SIZE * IMAGE_SIZE)
+
+    private val transformModel by lazy {
+        val options = FirebaseModelOptions.Builder()
+            .setLocalModelName(STYLE_TRANSFORM)
+            .build()
+        FirebaseModelInterpreter.getInstance(options)!!
+    }
+
+    private val transformOptions = FirebaseModelDataType.FLOAT32.let { dataType ->
+        FirebaseModelInputOutputOptions.Builder()
+            .setInputFormat(0, dataType, intArrayOf(1, IMAGE_SIZE, IMAGE_SIZE, PIXEL_SIZE))
+            .setOutputFormat(0, dataType, intArrayOf(1, IMAGE_SIZE, IMAGE_SIZE, PIXEL_SIZE))
+            .build()
+    }
+
+    init {
+        val transform = FirebaseLocalModel.Builder(STYLE_TRANSFORM)
+            .setAssetFilePath("pop_art_styler.tflite")
+            .build()
+        FirebaseModelManager.getInstance().registerLocalModel(transform)
+    }
+
+    fun transform(bitmap: Bitmap): Task<Bitmap> {
+        val inputs = FirebaseModelInputs.Builder()
+            .add(bitmap.toByteBuffer())
+            .build()
+        return transformModel.run(inputs, transformOptions)
+            .continueWith {
+                toBitmap(it.result!!.getOutput(0))
+            }
+    }
+
+    /**
+     * Writes Image data into a `ByteBuffer`.
+     */
+    @Synchronized
+    private fun Bitmap.toByteBuffer(): ByteBuffer {
+        val pixels = centerCropped(IMAGE_SIZE).toPixels()
+        val inputSize = FLOAT_TYPE_SIZE * IMAGE_SIZE * IMAGE_SIZE * PIXEL_SIZE
+        return ByteBuffer.allocateDirect(inputSize).apply {
+            order(ByteOrder.nativeOrder())
+            rewind()
+
+            for (pixel in pixels) {
+                putFloat((pixel and 0xFF).toFloat())        // B
+                putFloat((pixel shr 8 and 0xFF).toFloat())  // G
+                putFloat((pixel shr 16 and 0xFF).toFloat()) // R
+//                Timber.d("$pixel =" +
+//                        " ${(pixel shr 16 and 0xFF)}" +
+//                        " ${(pixel shr 8 and 0xFF)}" +
+//                        " ${(pixel and 0xFF)}")
+            }
+        }
+    }
+
+    private fun Bitmap.centerCropped(size: Int): Bitmap {
+        return Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888).also {
+            val frameToCropTransform = ImageUtils.getTransformationMatrix(
+                width, height,
+                size, size,
+                0, true
+            )
+            Canvas(it).drawBitmap(this, frameToCropTransform, null)
+        }
+    }
+
+    private fun Bitmap.toPixels(): IntArray {
+        getPixels(intValues, 0, width, 0, 0, width, height)
+        return intValues
+    }
+
+    private fun toBitmap(output: Array<Array<Array<FloatArray>>>): Bitmap {
+        var index = 0
+        for (i in 0 until IMAGE_SIZE) {
+            for (j in 0 until IMAGE_SIZE) {
+                intValues[index++] = (-0x1000000 or
+                        ((output[0][i][j][0] + IMAGE_OFFSET) * IMAGE_MEAN).toInt().shl(16) or
+                        ((output[0][i][j][1] + IMAGE_OFFSET) * IMAGE_MEAN).toInt().shl(8) or
+                        ((output[0][i][j][2] + IMAGE_OFFSET) * IMAGE_MEAN).toInt())
+            }
+        }
+        //val i = 0
+        //val j = 0
+        //val R = ((output[0][i][j][0] + IMAGE_OFFSET) * IMAGE_MEAN).toInt()
+        //val G = ((output[0][i][j][1] + IMAGE_OFFSET) * IMAGE_MEAN).toInt()
+        //val B = ((output[0][i][j][2] + IMAGE_OFFSET) * IMAGE_MEAN).toInt()
+        //timber.log.Timber.d("$i, $j = $R $G $B")
+        return Bitmap.createBitmap(IMAGE_SIZE, IMAGE_SIZE, Bitmap.Config.ARGB_8888).apply {
+            setPixels(intValues, 0, width, 0, 0, width, height)
+        }
+    }
+}
