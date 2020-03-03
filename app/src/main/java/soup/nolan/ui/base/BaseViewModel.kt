f40d56da27604aff0714bdfda08cf22f20211171
Sungyong An <soupyong@gmail.com>
2020-03-04 00:42:16 +0900

    Goodbye RxJava2

--- app/src/main/java/soup/nolan/ui/base/BaseViewModel.kt
+++ app/src/main/java/soup/nolan/ui/base/BaseViewModel.kt
@@ -1,19 +1,5 @@
 package soup.nolan.ui.base
 
 import androidx.lifecycle.ViewModel
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
 
-abstract class BaseViewModel : ViewModel() {
-
-    private val disposable = CompositeDisposable()
-
-    override fun onCleared() {
-        disposable.dispose()
-        super.onCleared()
-    }
-
-    protected fun Disposable.disposeOnCleared() {
-        disposable.add(this)
-    }
-}
+abstract class BaseViewModel : ViewModel()
