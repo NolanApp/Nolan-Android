a1a70c56d5128978d92a71800666f3ae36666445
SOUP <soupyong@gmail.com>
2020-05-26 20:36:38 +0900

    Stylize: Prepare grayscale

--- filter/stylize-common/src/main/java/soup/nolan/stylize/common/BitmapExtensions.kt
+++ filter/stylize-common/src/main/java/soup/nolan/stylize/common/BitmapExtensions.kt
@@ -1,7 +1,6 @@
 package soup.nolan.stylize.common
 
-import android.graphics.Bitmap
-import android.graphics.Canvas
+import android.graphics.*
 
 fun Bitmap.centerCropped(size: Int): Bitmap {
     return Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888).also {
@@ -17,4 +16,13 @@ fun Bitmap.centerCropped(size: Int): Bitmap {
 fun Bitmap.toPixels(intValues: IntArray): IntArray {
     getPixels(intValues, 0, width, 0, 0, width, height)
     return intValues
+}
+
+fun Bitmap.grayscale(): Bitmap {
+    val grayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+    val matrix = ColorMatrix().apply { setSaturation(0f) }
+    val filter = ColorMatrixColorFilter(matrix)
+    val paint = Paint().apply { colorFilter = filter }
+    Canvas(grayscale).drawBitmap(this, 0f, 0f, paint)
+    return grayscale
 }
