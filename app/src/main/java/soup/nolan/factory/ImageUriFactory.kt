9cdef917fc2a700ecbaea8b338446e76f17c4c81
SOUP <soupyong@gmail.com>
2020-08-18 15:16:25 +0900

    App: Remove pre-generated filter images

--- app/src/main/java/soup/nolan/factory/ImageUriFactory.kt
+++ app/src/main/java/soup/nolan/factory/ImageUriFactory.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.net.Uri
 import androidx.annotation.DrawableRes
 import androidx.core.content.FileProvider
+import androidx.core.net.toUri
 import soup.nolan.BuildConfig
 import soup.nolan.R
 import soup.nolan.model.CameraFilter
@@ -49,8 +50,12 @@ class ImageUriFactoryImpl(
     }
 
     override fun getFilterImageUri(filter: CameraFilter): Uri {
+        val saveDir = File(context.filesDir, FILTER_FILE_PATH)
+        if (!saveDir.exists()) {
+            saveDir.mkdirs()
+        }
+        return File(saveDir, filter.toFileName()).toUri()
         //TODO: Generate thumbnails dynamically and use the file uris
-        return getImageUri(filter.thumbnailResId)
     }
 
     private fun getImageUri(@DrawableRes resId: Int): Uri {
@@ -64,42 +69,12 @@ class ImageUriFactoryImpl(
         }
     }
 
-    private val CameraFilter.thumbnailResId: Int
-        @DrawableRes
-        get() = when (id) {
-            CameraFilter.OR.id -> R.drawable.default_image
-            CameraFilter.A01.id -> R.drawable.a01
-            CameraFilter.A02.id -> R.drawable.a02
-            CameraFilter.A03.id -> R.drawable.a03
-            CameraFilter.A04.id -> R.drawable.a04
-            CameraFilter.A05.id -> R.drawable.a05
-            CameraFilter.A06.id -> R.drawable.a06
-            CameraFilter.A07.id -> R.drawable.a07
-            CameraFilter.A08.id -> R.drawable.a08
-            CameraFilter.A09.id -> R.drawable.a09
-            CameraFilter.A10.id -> R.drawable.a10
-            CameraFilter.A11.id -> R.drawable.a11
-            CameraFilter.A12.id -> R.drawable.a12
-            CameraFilter.A13.id -> R.drawable.a13
-            CameraFilter.A14.id -> R.drawable.a14
-            CameraFilter.A15.id -> R.drawable.a15
-            CameraFilter.A16.id -> R.drawable.a16
-            CameraFilter.A17.id -> R.drawable.a17
-            CameraFilter.A18.id -> R.drawable.a18
-            CameraFilter.A19.id -> R.drawable.a19
-            CameraFilter.A20.id -> R.drawable.a20
-            CameraFilter.A21.id -> R.drawable.a21
-            CameraFilter.A22.id -> R.drawable.a22
-            CameraFilter.A23.id -> R.drawable.a23
-            CameraFilter.A24.id -> R.drawable.a24
-            CameraFilter.A25.id -> R.drawable.a25
-            CameraFilter.A26.id -> R.drawable.a26
-            else -> throw IllegalArgumentException("CameraFilter($id) is invalid.")
-        }
-
     companion object {
         private const val FILTER_FILE_PATH = "image_manager/filter/"
         private const val FILTER_FILE_CAMERA = "camera_image.jpg"
-        private val FILTER_FILE_ORIGIN = "filter_${CameraFilter.OR.id}.jpg"
+
+        private fun CameraFilter.toFileName(): String {
+            return "filter_$id.jpg"
+        }
     }
 }
